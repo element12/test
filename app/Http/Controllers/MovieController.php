be18efb7cbcc2fbd4113e3ae24d21ff4b76211dc
ElBomberitoFeliz <jeisonortiz0016@gmail.com>
2021-02-25 16:01:26 -0500

    env

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(movie $movie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, movie $movie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(movie $movie)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_02_25_191106_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->string('time_duration',100);
+            $table->string('category',250);
+            $table->string('link_yt',500);
+            $table->date('premiere_date');
+            $table->boolean('view_movie');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
